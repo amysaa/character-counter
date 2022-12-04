9ee62efcf5a74c0914ff75e83f03cd407194b404
Amisha Verma <amishaverma2805@gmail.com>
2022-12-04 11:52:33 +0530

    app completed

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("name");
+const charval = document.getElementById("textarea");
+let totalCount = document.getElementById("total-counter");
+let remainingCount = document.getElementById("remaining-counter");
+
+let userChar = 0;
+
+const updateCounter = () => {
+  userChar = charval.value.length;
+
+  totalCount.innerText = userChar;
+
+  remainingCount.innerText = 150 - userChar;
+};
+
+charval.addEventListener("keyup", () => updateCounter());
+
+// to copy the text
+const copyText = () => {
+  charval.select();
+  charval.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(charval.value);
+};
